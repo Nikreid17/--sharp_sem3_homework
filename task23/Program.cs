b7ae81236464be9c436454286fbdcd5027a2bbb4
Nickolay <nik5te@yandex.ru>
2023-02-15 16:36:00 +0300

    таблица кубов до n

---
+++ task23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int m = N;
+if (N < 0)
+{
+    i = N;
+    m = -N;
+    N = -1;
+}
+int[] result = new int[m];
+for (int j = 0; i <= N; i++)
+{
+    result[j] = i * i * i;
+    j++;
+}
+for (int j = 0; i < m; j++)
+{
+    Console.Write($"{result[j]} ");
+}
